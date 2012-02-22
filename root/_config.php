a9296eb26b15ec344fee98800cb2fa73ed44dbd9
Eugene Ustupkin <eugene.ustupkin@gmail.com>
2012-02-23 02:45:23 +0400

    reload

--- root/_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$objTplMan->setTemplate($arrUri[$i],TPLPATH.'main.tpl.php');
-$objNavMan->setTemplate(array('general'),TPLPATH.'_nav_general.xsl');
-$objNavMan->setTemplate(array('ad','marketing'),TPLPATH.'_nav_left.xsl');
-$objNavMan->setTemplate(array('ad_types'),TPLPATH.'_nav_right.xsl');
-
-
-?>
